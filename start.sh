9972f9f7c8e4aa9981302ac7dd7f20ed4c45e858
Slyces <simon.lassourreuille@gmail.com>
2017-06-18 23:04:01 +0200

    added gunicorn working for deployment

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Start Gunicorn processes
-echo Scribe -- Starting Gunicorn.
-exec gunicorn scribe.wsgi:application \
+echo Scribes -- Starting Gunicorn.
+cd Django
+exec gunicorn scribes.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
