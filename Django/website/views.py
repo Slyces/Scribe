316777cfb31dcb5842e27f2a36ee47225c2dbb2d
Slyces <simon.lassourreuille@gmail.com>
2017-07-20 22:29:46 +0200

    register page

--- Django/website/views.py
+++ Django/website/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-# Temp
-from django.http import HttpResponse
-from django.contrib.auth.views import login_required
+from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic.base import TemplateView
+from django.template import RequestContext
+from django.db import transaction
+from .forms import UserForm, ProfileForm
 
 
-class IndexView(TemplateView):
+class IndexView(View):
     template_name = 'website/index.html'
+
+
+@transaction.atomic
+def register(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, prefix='user')
+        profile_form = ProfileForm(request.POST, prefix='userprofile')
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            userprofile = profile_form.save(commit=False)
+            userprofile.user = user
+            userprofile.save()
+            return redirect('settings:profile')
+    else:
+        user_form = UserForm(prefix='user')
+        profile_form = ProfileForm(prefix='userprofile')
+    return render(request, 'registration/register.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form,
+                   })
